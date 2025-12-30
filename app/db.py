70414e37dace836ba02e10587e0e70e9b734e1d5
johanan26 <jseeruthen26@outlook.com>
2025-12-30 20:27:02 +0000

    implement the worker for doing tasks

--- app/db.py
+++ app/db.py
@@ -1,10 +1,12 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app.db")
 
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
 
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
-SessionLocal = sessionmaker(bind=engine, expire_on_commit=False)
+session_local = sessionmaker(bind=engine, expire_on_commit=False)
+async_session = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
